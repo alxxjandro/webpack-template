59ddd185d7a8464c48086e67af2d365900e6cc47
Alonso Alejandro Alarcón Parra <155941412+alxxjandro@users.noreply.github.com>
2025-02-17 12:59:55 -0600

    Update webpack.common.js
    Added the html template

--- webpack.common.js
+++ webpack.common.js
@@ -7,9 +7,21 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Production",
+      template: "./src/template.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
